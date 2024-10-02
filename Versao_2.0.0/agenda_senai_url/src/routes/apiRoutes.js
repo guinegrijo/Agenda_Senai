e7a15d5a5a9ca75878388ced12656358dbbe8a28
prieloizee <priscila.e.paulino@aluno.senai.br>
2024-10-02 16:09:07 -0300

    02/10

--- Versao_2.0.0/agenda_senai_url/src/routes/apiRoutes.js
+++ Versao_2.0.0/agenda_senai_url/src/routes/apiRoutes.js
@@ -1,4 +1,11 @@
 //importar a função Router do express (Router é usado para definir rotas específicas da aplicação
 const router = require('express').Router()
 
+const controllerAgenda_Senai = require ("../controllers/controllerAgenda_Senai")
+
+router.post('/cadastro',controllerAgenda_Senai.validaçãodeCadastro);
+router.get('/cadastro',controllerAgenda_Senai.getAllCadastro);
+router.put('/cadastro',controllerAgenda_Senai.updateCadastro);
+router.delete('/cadastro/:cpf',controllerAgenda_Senai.deleteCadastro);
+
 module.exports = router
