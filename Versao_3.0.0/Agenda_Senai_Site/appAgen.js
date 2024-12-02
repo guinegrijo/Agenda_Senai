b374d00ebc12e806439f0f2d85d332a64d5eda49
prieloizee <priscila.e.paulino@aluno.senai.br>
2024-12-02 09:23:03 -0300

    agenda senai

--- Versao_3.0.0/Agenda_Senai_Site/appAgen.js
+++ Versao_3.0.0/Agenda_Senai_Site/appAgen.js
@@ -1,50 +1,8 @@
 //document.addEventListener("DOMContentLoaded", getAllSala);
 document.addEventListener("DOMContentLoaded", getAllSalaSelect);
 
-function getAllSala() {
-  fetch("http://localhost:5000/Agenda_Senai/sala", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return response.json().then((err) => {
-        throw new Error(err.error);
-      });
-    })
-    .then((data) => {
-      const userList = document.getElementById("sala-tabela");
-      userList.innerHTML = ""; //Limpa a lista existente
-
-      data.salas.forEach((sala) => {
-        const tr = document.createElement("tr");
-
-        const tdNumeroSala = document.createElement("td");
-        tdNumeroSala.textContent = sala.numero_da_sala;
-        tr.appendChild(tdNumeroSala);
-
-        const tdCapacidade = document.createElement("td");
-        tdCapacidade.textContent = sala.capacidade;
-        tr.appendChild(tdCapacidade);
-
-        const tdDescricao = document.createElement("td");
-        tdDescricao.textContent = sala.descricao;
-        tr.appendChild(tdDescricao);
-
-        userList.appendChild(tr);
-      });
-    })
-    .catch((error) => {
-      alert("Erro ao obter salas" + error.message);
-      console.error("Erro:", error.message);
-    });
-}
-
 function getAllSalaSelect() {
+
   fetch("http://localhost:5000/Agenda_Senai/sala", {
     method: "GET",
     headers: {
